6d16d1966d7a6c0610d55f0d5f43080afd176e2b
shyam <skshyampatel@gmail.com>
2019-01-31 20:13:23 -0500

    without sql streaming

--- Spark/lecture4/exe5.py
+++ Spark/lecture4/exe5.py
@@ -1,5 +1,7 @@
 
 import mysql.connector
+#Install connector using: sudo pip install mysql-connector-python
+
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 
@@ -8,16 +10,23 @@ ssc = StreamingContext(sc, 2)
 
 lines = ssc.socketTextStream("localhost", 9999)
 
-#lines.foreachRDD(lambda x : x.saveAsTextFile("/mnt/d/exe4"))
-def saveResult(rdd):
-    host=
-    user=
-    passwd=
-    database=
-    mydb = mysql.connector.connect(host, user, passwd, database)
-    for record in rdd:
+def saveResult(part):
+  hostname="34.218.225.90"
+  usr="spark"
+  pwd="spark"
+  db="spark_db"
+  tbl="test_tbl"
+  sql = "INSERT INTO test_tbl (data) VALUES (%s)"
+  import mysql.connector
+  mydb = mysql.connector.connect(host=hostname, user=usr, password=pwd, database=db)
+  mycursor = mydb.cursor()
+  for record in part:
+    val = (record,)
+    mycursor.execute(sql, val)
+  mydb.commit()
+  mydb.close()
 
-
+lines.foreachRDD(lambda x: x.foreachPartition(saveResult))
 
 ssc.start()             # Start the computation
-ssc.awaitTermination() 
+ssc.awaitTermination()
